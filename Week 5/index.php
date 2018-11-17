2ce04204f5b06d05011af2546264a9a596aaead1
Anamitra Musib <ana1998musib@gmail.com>
2018-11-18 00:17:38 +0530

    Add files via upload

---
+++ Week 5/index.php
@@ -0,0 +1,19 @@
+
+<title>Anamitra Musib PHP</title>
+<h1>Anamitra Musib PHP</h1>
+<p><?php
+	print "The SHA256 hash of \"Anamitra Musib\" is ".hash('sha256', 'Anamitra Musib');
+?></p>
+<pre>ASCII ART:
+	       ***
+	      *****
+	     **	  **
+	    **     **
+	   ***********
+	  *************
+	 **           **
+	**             **
+</pre>
+<a href="fail.php">Click here to check the error setting</a>
+<br>
+<a href="check.php">Click here to cause a traceback</a>
