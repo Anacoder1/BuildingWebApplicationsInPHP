d5048d043dc910e9be1e13dc8a61fcb33eb62559
Anamitra Musib <ana1998musib@gmail.com>
2018-11-18 00:18:15 +0530

    Add files via upload

---
+++ Week 6/guess.php
@@ -0,0 +1,33 @@
+<html>
+<body>
+<title> Anamitra Musib 41884c79 </title>
+<h1> Welcome to my guessing game</h1>
+
+<?php
+$correctnumber=26; //change this number //
+if($_GET['guess'])
+{
+  if (is_numeric($_GET['guess'])===FALSE)
+ {
+echo "Your guess is not a number";
+}
+else if ($_GET['guess']<$correctnumber)
+ {
+echo "Your guess is too low";
+}
+else if ($_GET['guess']>$correctnumber)
+ {
+echo "Your guess is too high";
+}
+else if ($_GET['guess']==$correctnumber)
+ {
+echo "Congratulations - You are right";
+}
+}
+else
+{
+echo "Missing guess parameter <br> Your guess is too short";
+}
+?>
+</body>
+</html>
