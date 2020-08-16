1327e4a878fde80ae9f2ca292270ec3866f326d6
Samyuktha1410 <50766407+Samyuktha1410@users.noreply.github.com>
2020-08-16 09:44:16 +0530

    Update index.php

--- Week 5/index.php
+++ Week 5/index.php
@@ -1,8 +1,8 @@
 
-<title>Anamitra Musib PHP</title>
-<h1>Anamitra Musib PHP</h1>
+<title>Samyuktha PHP</title>
+<h1>Samyuktha PHP</h1>
 <p><?php
-	print "The SHA256 hash of \"Anamitra Musib\" is ".hash('sha256', 'Anamitra Musib');
+	print "The SHA256 hash of \"Samyuktha\" is ".hash('sha256', 'Anamitra Musib');
 ?></p>
 <pre>ASCII ART:
 	       ***
